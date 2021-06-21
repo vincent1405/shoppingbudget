e1f45bd0e1c60e37ed225f414fceb47e65abe3eb
vincent1405 <vincent.dewisme@gmail.com>
2021-06-21 20:10:28 +0200

    Swagger added

--- src/ShoppingBudget.WebApi/Startup.cs
+++ src/ShoppingBudget.WebApi/Startup.cs
@@ -1,19 +1,11 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ShoppingBudget.WebApi
 {
@@ -59,6 +51,7 @@ namespace ShoppingBudget.WebApi
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapSwagger();
             });
         }
     }
